f7393fb0c903b63f84015e82c5ac76248f831a13
Romanovich Maxim <maximqq123@gmail.com>
2024-03-17 19:48:09 +0300

    Add permissions for unauthorized users

--- news_proj/news/views.py
+++ news_proj/news/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from news.models import News, Category
 from news.serializers import NewsSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class NewsViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
